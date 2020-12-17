bffc4f0baedd9b37189a52a2fd0f367db697773e
usertutu <484027291@qq.com>
2020-12-17 16:31:25 +0800

    初始化

---
+++ src/app.config.ts
@@ -0,0 +1,11 @@
+import { AppConfig } from "remax/wechat";
+
+const config: AppConfig = {
+  pages: ['pages/index/index'],
+  window: {
+    navigationBarTitleText: 'Remax',
+    navigationBarBackgroundColor: '#282c34'
+  }
+};
+
+export default config;
